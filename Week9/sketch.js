ae220bbb765af130e4c90368ee1cc55d3bb40d63
pragmatprevar <leiarose@protonmail.com>
2024-03-31 21:12:38 -0600

    week 9

---
+++ Week9/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(200);
+  circle(200,250,150);
+  square(127,125,145);
+  circle(170,200,30);
+  circle(230,200,30);
+  triangle(125,125,200,100,270,125)
+  line(175,275,225,280)
+  triangle(175,255,200,245,225,255)
+  point(200,250)
+  rect(115,200,17,40)
+  rect(265,200,17,40)
+  text('A. Dean',10,375)
+  text('Shapes Make The Man',130,30)
+}
